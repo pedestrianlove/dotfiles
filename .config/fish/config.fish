2bf7435311b88e618c9e691426c674bbb85bb5c2
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2024-02-05 23:27:44 +0800

    Add fish config files.

---
+++ .config/fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Remove default greeting msg
+    set -g fish_greeting
+
+    # SSH Aliases
+    function lab
+	ssh jsl@140.128.101.141
+    end
+    function oracle
+	ssh -i ~/.ssh/oracle_rsa opc@158.101.88.245
+    end
+
+    # Utilities
+    alias v="nvim"
+    alias vim="nvim"
+    alias t="tmux"
+    alias ta="tmux a"
+    alias pa="php artisan"
+    alias sail="./vendor/laravel/sail/bin/sail"
+    alias ls="ls --color=auto"
+    alias ll="ls -la"
+    alias pstorm="phpstorm"
+    alias phpdoc="docker run --rm -v $PWD:/data phpdoc/phpdoc:3"
+    alias config="vim ~/.config/sway/config"
+    alias thu="sudo openconnect --protocol=nc thuvpn.twaren.net"
+
+    # Custom keybindings
+    bind \cf ~/Work/tmux-sessionizer
+end
