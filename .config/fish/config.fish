13b5ef9edb8c29d555e0f8d63ca61f22116c1ced
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2026-01-20 21:53:25 +0800

    chore: fix conflict

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,7 +21,18 @@ if status is-interactive
     alias config="vim ~/.config/hypr/hyprland.conf"
     alias thu="sudo openconnect --protocol=nc thuvpn.twaren.net"
     alias nthu="sudo openconnect --protocol=nc nthu.twaren.net"
+    alias gpu1="ssh gpu1"
+    alias gpu2="ssh gpu2"
+    alias gpu3="ssh gpu3"
+    alias gpu4="ssh gpu4"
+    alias gpu5="ssh gpu5"
+    alias nthuk1="ssh jsl@k8snode1"
+    alias nthuk2="ssh jsl@k8snode2"
+    alias nthulabtr="ssh jsl@labtr"
+    alias nthulabpc="ssh jsl@labpc"
+    alias orient="sudo ssh -L 80:192.168.20.116:80 -L 443:192.168.20.116:443 service@service-h310m-s2p-2-0"
 
     # Custom keybindings
     bind \cf ~/.config/tmux-sessionizer
 end
+fish_add_path /home/jsl/.pixi/bin
